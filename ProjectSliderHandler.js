aeff2aa96286aa322eed1039545a1a14154be479
AkashLM <malekarakash70@gmail.com>
2022-03-21 21:54:13 +0530

    Design for project corousal completed and basic roller completed. remaining 3 project deigns in corousal

---
+++ ProjectSliderHandler.js
@@ -0,0 +1,33 @@
+const FirstDivBox=document.getElementById("Right-Project_slider");
+
+function BoxSliderMovieFetcher(){
+    
+    FirstDivBox.style.marginTop="0px";
+    FirstDivBox.style.transition="3s";
+
+};
+
+function BoxSliderInstagram(){
+
+    FirstDivBox.style.marginTop="-550px";
+    FirstDivBox.style.transition="3s";
+}
+
+function BoxSliderMakeUpProject(){
+
+    FirstDivBox.style.marginTop="-1100px";
+    FirstDivBox.style.transition="3s";
+}
+
+
+function BoxSlideStickyNotes(){
+
+    FirstDivBox.style.marginTop="-1650px";
+    FirstDivBox.style.transition="3s";
+}
+
+function BoxSlideTextToSpeech(){
+
+    FirstDivBox.style.marginTop="-2200px";
+    FirstDivBox.style.transition="3s";
+}
